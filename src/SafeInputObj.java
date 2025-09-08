7298cf7fc216af855e991071bd609338b4836442
scarlaos <scarlaos@mail.uc.edu>
2025-09-07 20:54:50 -0400

    com

---
+++ src/SafeInputObj.java
@@ -0,0 +1,190 @@
+import java.util.Scanner;
+
+public class SafeInputObj {
+    private Scanner pipe;
+
+    public SafeInputObj() {
+        this.pipe = new Scanner(System.in);
+    }
+
+    public String getNonZeroLenString(String prompt)
+    {
+        String retString = "";
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        }while(retString.length() == 0); // until we have some characters
+
+        return retString;
+
+    }
+
+    public int getRangedInt(String prompt, int low, int high)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if(pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+    public int getInt(String prompt)
+    {
+        int retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter an int: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+    public double getRangedDouble(String prompt, int low, int high)
+    {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + "[" + low + "-" + high + "]: ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a double: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+    public double getDouble(String prompt)
+    {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a double: " + trash);
+            }
+        }while(!done);
+
+        return retVal;
+    }
+
+    public boolean getYNConfirm(String prompt)
+    {
+        boolean retVal = true;
+        String response = "";
+        boolean gotAVal = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + " [Y/N] ");
+            response = pipe.nextLine();
+            if(response.equalsIgnoreCase("Y"))
+            {
+                gotAVal = true;
+                retVal = true;
+            }
+            else if(response.equalsIgnoreCase("N"))
+            {
+                gotAVal = true;
+                retVal = false;
+            }
+            else
+            {
+                System.out.println("You must answere [Y/N]! " + response );
+            }
+
+        }while(!gotAVal);
+
+        return retVal;
+    }
+
+    public String getRegExString(String prompt, String regExPattern)
+    {
+        String response = "";
+        boolean gotAVal = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            response = pipe.nextLine();
+            if(response.matches(regExPattern))
+            {
+                gotAVal = true;
+            }
+            else
+            {
+                System.out.println("\n" + response + " must match the pattern " + regExPattern);
+                System.out.println("Try again!");
+            }
+
+        }while(!gotAVal);
+
+        return response;
+    }
+}
